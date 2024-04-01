db9ac67616a2ceeaefe771c91f3e999a20b44d7e
Xane <mafimbulvetr@gmail.com>
2024-04-01 15:19:01 +0100

    which account am I on anyway

--- tests.py
+++ tests.py
@@ -2,5 +2,6 @@ from diagram_generator_dan import generate_diagram as diagram_dan
 from diagramfile import diagram as diagram_ella
 
 if __name__ == "__main__":
+    # making sure both diagrams print the same code
     diagram_dan(9, 7)
     diagram_ella(9, 7)
