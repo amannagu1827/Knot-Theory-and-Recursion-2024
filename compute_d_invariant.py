c1bd6636a2628d7191615b9ecd79ffb0932fec72
Susananyuan <164250603+Susananyuan@users.noreply.github.com>
2024-04-14 16:05:09 +0800

    Add files via upload

---
+++ compute_d_invariant.py
@@ -0,0 +1,9 @@
+def calculate_d_invariant(p, q, i):
+    if p == 1:
+        return 0
+    p_mod_q = p % q
+    i_mod_p = i % p
+    if p_mod_q == 0:
+        return 0
+    else:
+        return ((2 * i + 1 - p - q) ** 2 - p * q) / (4 * p * q) - calculate_d_invariant(q, p_mod_q, i_mod_p)
