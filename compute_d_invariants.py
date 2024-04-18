b653321b992d56867f5b0bec75318bfbbfc62b54
danflett03 <164231443+danflett03@users.noreply.github.com>
2024-04-18 11:28:47 +0100

    Update compute_d_invariants.py
    Changed range(p+q) to range(p)

--- compute_d_invariants.py
+++ compute_d_invariants.py
@@ -3,6 +3,6 @@ from compute_d_invariant import calculate_d_invariant
 
 def compute_d_invariants(p, q):
     d_invariants = []
-    for i in range(p + q):
+    for i in range(p):
         d_invariants.append(calculate_d_invariant(p, q, i))
     return d_invariants
