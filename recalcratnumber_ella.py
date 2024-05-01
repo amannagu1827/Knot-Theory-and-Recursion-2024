9fffaff9f38dfcec8f8076c5cf7943065442227f
ellawomack <164229441+ellawomack@users.noreply.github.com>
2024-05-01 13:31:08 +0100

    Update recalcratnumber_ella.py

--- recalcratnumber_ella.py
+++ recalcratnumber_ella.py
@@ -3,11 +3,11 @@ cont_frac1 = []             #assigning an array; note we use cont_frac1 as our n
 def recalc_1(cont_frac1): 
     
     if not cont_frac1:
-        return 0, 1         #if the continued fraction is empty return 0
+        return 0, 1         #if the continued fraction array is empty return 0
     
     p, q = cont_frac1[-1], 1
     
-    for term in cont_frac1[-2::-1]:     #iterating in reverse order
-        p, q = term * p + q, p
+    for value in cont_frac1[-2::-1]:     #iterating in reverse order
+        p, q = value * p + q, p
         
     return p, q
